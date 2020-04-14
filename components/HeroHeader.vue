8aac65671cad8b351f8b534af6b59e54b435f4e7
Mike Tierce <mike.tierce@gmail.com>
2020-04-13 22:27:19 -0500

    init

---
+++ components/HeroHeader.vue
@@ -0,0 +1,33 @@
+<template>
+
+      <v-layout row style="height: 90vh" class="pa-0">
+    <v-flex xs="12" class="pa-0 pt-5">
+        <v-img
+        width="100%"
+    class=""
+    alt="o"
+    :src="require('~/static/header-image.png')"
+  >
+        </v-img>
+        <v-img
+        width="100%"
+    class=""
+    style="margin-top: -10em; margin-left: 10em;"
+    alt="o"
+    :src="require('~/static/truck.png')"
+  >
+        </v-img>
+    </v-flex>
+      </v-layout>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
